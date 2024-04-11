ea3e9613a646a366e53b8350dd3df6a293c7c681
LashGAMX <alber_199648@hotmail.com>
2024-04-11 16:52:38 -0600

    commit

--- src/components/TemporaryDrawer.jsx
+++ src/components/TemporaryDrawer.jsx
@@ -1,3 +1,4 @@
+import { hover } from '@testing-library/user-event/dist/hover';
 import * as React from 'react';
 
 
@@ -5,22 +6,37 @@ export default function TemporaryDrawer() {
 
   return (
     <>
-     
-     <div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">MENU</h5>
-        <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div className="offcanvas-body">
-        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-
-        </ul>
-        
-      </div>
-    </div>
+ <div
+            className="offcanvas offcanvas-start"
+            tabindex="-1"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+          >
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                Menu
+              </h5>
+              <button
+                type="button"
+                className="btn-close text-reset"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav justify-content-start flex-grow-1 pe-3">
+                <li className="nav-item">
+                  <a
+                    className="nav-link active bgMenu"
+                    aria-current="page"
+                    href="#"
+                  >
+                    Home
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
     </>
   );
 }
--- src/templates/NavBar.jsx
+++ src/templates/NavBar.jsx
@@ -8,10 +8,10 @@ import TemporaryDrawer from '../components/TemporaryDrawer'
 export const NavBar = () => {
   return (
     <>
-    <ul>
-        <li><a className="active text-dark " data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" href="#menu"><img src={menu} width="20" /></a></li>
+    <ul className='border-bottom'>
+        <li><a className="active text-dark"  data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" href="#menu"><img src={menu} width="20" /></a></li>
         <li><a href="#home"><img src={home} width="20" alt='home'/></a></li>
-        <li style={{float:"right",margin:"5px"}}><div className='circular--portrait '><img src={perfil} /></div></li>
+        <li style={{float:"right",margin:"5px"}}><div className='circular--portrait '><img src={perfil} alt='perfil' /></div></li>
     </ul>
 
     <TemporaryDrawer/>
